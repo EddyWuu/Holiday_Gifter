1cfe1280c3dd66c16dcdd2b4088f1bdf3d2801f1
Edmond Wu <ducks@Edmonds-MacBook-Pro.local>
2024-09-16 19:06:33 -0700

    setup

---
+++ Holiday_Gifter/Holiday_GifterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Holiday_GifterApp.swift
+//  Holiday_Gifter
+//
+//  Created by Edmond Wu on 2024-09-16.
+//
+
+import SwiftUI
+
+@main
+struct Holiday_GifterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Holiday_GifterTests/Holiday_GifterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Holiday_GifterTests.swift
+//  Holiday_GifterTests
+//
+//  Created by Edmond Wu on 2024-09-16.
+//
+
+import Testing
+@testable import Holiday_Gifter
+
+struct Holiday_GifterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
