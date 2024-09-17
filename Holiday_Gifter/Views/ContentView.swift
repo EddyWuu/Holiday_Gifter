bf2f0ab39be2ccf37707f73d7439f01e60dce84f
Edmond Wu <edmond.wuca@gmail.com>
2024-09-16 19:36:29 -0700

    more changes

---
+++ Holiday_Gifter/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Holiday_Gifter
+//
+//  Created by Edmond Wu on 2024-09-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world! This is a SwiftUI app.")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Holiday_Gifter/Views/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeView.swift
+//  Holiday_Gifter
+//
+//  Created by Edmond Wu on 2024-09-16.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world! This is a SwiftUI app.")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
