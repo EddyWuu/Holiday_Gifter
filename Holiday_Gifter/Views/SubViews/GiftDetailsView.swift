4cfbc7a740ca788540909c6b602b7063ebdcc0be
Edmond Wu <edmond.wuca@gmail.com>
2024-09-27 14:22:35 -0700

    sept 27, 2024

--- Holiday_Gifter/Views/SubViews/GiftDetailsView.swift
+++ Holiday_Gifter/Views/SubViews/GiftDetailsView.swift
@@ -11,30 +11,49 @@ struct GiftDetailsView: View {
     
     var recipient: Recipient
     @Binding var showGiftDetails: Bool
+    @State private var addGiftSheet: Bool = false
     
     var body: some View {
             
         NavigationView {
-            List {
-                ForEach(recipient.gifts) { gift in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(gift.name)
-                                .font(.headline)
-                            Text("Price($): \(gift.price, specifier: "%.2f")")
-                                .font(.subheadline)
-                                .foregroundColor(.secondary)
+            VStack {
+                List {
+                    ForEach(recipient.gifts) { gift in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(gift.name)
+                                    .font(.headline)
+                                Text("Price($): \(gift.price, specifier: "%.2f")")
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                            }
+                            Spacer()
                         }
-                        Spacer()
-                    }
-                    .swipeActions(edge: .trailing, allowsFullSwipe: true) {
-                        Button(role: .destructive) {
-                            // TODO, to remove from gifts
-                        } label: {
-                            Label("Delete", systemImage: "trash")
+                        .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                            Button(role: .destructive) {
+                                // TODO, to remove from gifts
+                            } label: {
+                                Label("Delete", systemImage: "trash")
+                            }
                         }
                     }
                 }
+                
+                Spacer()
+                
+                Button(action: {
+                    addGiftSheet = true
+                }) {
+                    Text("Add Gift")
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal)
+                
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
